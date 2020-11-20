d39f781d69c5b66bbbea736f43331e57ca76e13b
Galaa1999 <53171848+Galaa1999@users.noreply.github.com>
2020-11-20 15:09:11 +0800

    a
    a

--- project4/p4.jsx
+++ project4/p4.jsx
@@ -1,9 +1,73 @@
-import { React} from "react";
-import State from "./components/states/States"
-import Example from "./components/example/Example"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import State from './components/states/States'
+import Example from './components/example/Example'
 
-ReactDOM.render(
-  console.log("bbbbbbbbbbbbb"),
-    <States />,
-    document.getElementById('reactapp1'),
+import Header from './components/header/Header'
+
+  function Swi(pro) {
+    
+    const boole = pro.boole;
+    if (boole) {
+      return <State/>;
+    }
+    return <Example />;
+  }
+
+
+function ExampleB(pro) {
+    return (
+      <button onClick={pro.onClick}>
+        Go to State
+      </button>
+    );
+  }
+  
+  function Stateb(pro) {
+    return (
+      <button onClick={pro.onClick}>
+        Go to Example
+      </button>
+    );
+  }
+
+  class Switche extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleStateClick = this.handleStateClick.bind(this);
+      this.handleExampleClick = this.handleExampleClick.bind(this);
+      this.state = {boole: false};
+    }
+  
+    handleStateClick() {
+      this.setState({boole: true});
+    }
+  
+    handleExampleClick() {
+      this.setState({boole: false});
+    }
+  
+    render() {
+      const boole = this.state.boole;
+      let button;
+  
+      if (boole) {
+        button = <Stateb onClick={this.handleExampleClick} />;
+      } else {
+        button = <ExampleB onClick={this.handleStateClick} />;
+      }
+  
+      return (
+        <div>
+          <Header/>
+          {button}
+          <Swi boole={boole} />
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Switche />,
+    document.getElementById('reactapp4')
   );
